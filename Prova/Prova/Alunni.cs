a039d11420abb6311c039a293c7ed67954112de5
gabrielcannonito <71811571+gabrielcannonito@users.noreply.github.com>
2020-09-24 09:13:18 +0200

    Aggiunta del progetto visual studio

---
+++ Prova/Prova/Alunni.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    class Alunni
+    {
+        private string nome;
+        private string cognome;
+        private int nascita;
+        private string codice_fiscale;
+        private string luogo;
+        private string classe;
+
+        public Alunni(string nome,string cognome,int nascita, string codice_fiscale,string luogo,string classe)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.nascita = nascita;
+            this.codice_fiscale = codice_fiscale;
+            this.luogo = luogo;
+            this.classe = classe;
+        }
+    }
+}
---
+++ Prova/Prova/Elenco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    class Elenco
+    {
+        private List<Alunni> elencoAlunni;
+        public Elenco()
+        {
+            elencoAlunni = new List<Alunni>();
+        }
+        public void Aggiungi (Alunni unalunno)
+        {
+            elencoAlunni.Add(unalunno);
+        }
+    }
+}
